cff5db693d8b290c4530c204edba6edecc322ee6
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-10-10 20:08:19 +0600

    Add files via upload

---
+++ HACKS.lua
@@ -0,0 +1,500 @@
+function F2()
+F12 = gg.multiChoice({
+"🔥 ᴀɴᴛᴇɴᴀ ʜᴀᴄᴋ",
+"🔥 ʟᴇss ʀᴇᴄᴏɪʟ",
+"🔥 ɴᴏ ʀᴇᴄᴏɪʟ",
+"🔥 ᴀɪᴍʙᴏᴛ",
+"🔥 ʜᴇᴀᴅsʜᴏᴛ ",
+"🔥 x ᴇғғᴇᴄᴛ ",
+"🔥 sᴍᴀʟʟ ᴄʀᴏᴏsᴀɪʀ",
+"🔥 ɪɴsᴛᴀɴᴛ ʜɪɴᴛ",
+"🔥 ᴏɴᴇ ꜱʜᴏᴛ ᴋɪʟʟ (ɴᴏᴛ ꜱᴀꜰᴇ)",
+"🔥 ɪᴘᴀᴅ ᴠɪᴇᴡ",
+"🔥 ʙʟᴀᴄᴋ sᴋʏ",
+"🔥 ɴᴏ ғᴏɢ",
+"🔥 ʙʟᴜᴇ ғᴏɢ",
+"🔥 ꜱᴛᴀɴᴅ ꜱᴄᴏᴘᴇ",
+"🔥 ꜱɪᴛ ꜱᴄᴏᴘᴇ",
+"🔥 ɴᴏ ꜱʜᴀᴋᴇ",
+"🔥 ɴᴏ ɢʀᴀꜱꜱ",
+"🔥 ᴍᴀɢɪᴄ ʙᴜʟʟᴇᴛ",
+"ᴥ︎︎︎ ʙᴀᴄᴋ",
+}, nil, (os.date("%A %d %B %Y %H:%M%p\n")))
+if F12 == nil then
+  else
+  if F12[1] == true then
+      ANTENA()
+    end
+    if F12[2] == true then
+      LESSRECOIL()
+    end
+    if F12[3] == true then
+      NORECOIL()
+    end
+    if F12[4] == true then
+      A4()
+    end
+    if F12[5] == true then
+      A5()
+    end
+    if F12[6] == true then
+      A6()
+    end
+    if F12[7] == true then
+      A7()
+    end
+    if F12[8] == true then
+      A8()
+    end
+    if F12[9] == true then
+      A9()
+    end
+      if F12[10] == true then
+      N1()
+    end
+    if F12[11] == true then
+      N2()
+    end
+    if F12[12] == true then
+      N3()
+    end
+    if F12[13] == true then
+      N4()
+    end
+    if F12[14] == true then
+      STANDSCOPE()
+    end
+    if F12[15] == true then
+      SITSCOPE()
+    end
+    if F12[16] == true then
+      shake()
+    end
+    if F12[17] == true then
+      grass()
+    end
+    if F12[18] == true then
+      magic()
+    end
+    if F12[19] == true then
+      Home()
+    end
+    end
+PUBGMH = -1
+end
+
+function ANTENA()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("88.50576019287F;87.27782440186F;1F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(6)
+gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("𝙰𝙽𝚃𝙴𝙽𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function LESSRECOIL()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-2.786982e28;-2.2673448e24;-1.13688735e-13", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-2.786982e28", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.searchNumber("-2.8111605e28;-3.7444097e28;-1.1144502e28;128.0::", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-1.1144502e28", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Less Recoil Activated @iorihack")
+end
+
+function NORECOIL()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-2.786982e28;-2.2673448e24;-1.13688735e-13", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-2.786982e28", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.searchNumber("-2.8111605e28;-3.7444097e28;-1.1144502e28;128.0::", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-1.1144502e28", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.searchNumber("-6.1549454e27;1.8638966e-20;-1.1144502e28;0::", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-1.1144502e28", 16, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("𝙽𝙾 𝚁𝙴𝙲𝙾𝙸𝙻 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A4()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-290,174,237", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("-290,174,237", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineAddress("E00", -1, gg.TYPE_DWORD, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("𝙰𝙸𝙼𝙱𝙾𝚃 100𝙼 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A5()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber(1091779028, gg.TYPE_DWORD, nil, nil, nil, nil)
+gg.getResults(0)
+gg.getResultsCount()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("0.10000000149;64.50088500977", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.refineNumber("0.10000000149", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(50)
+gg.editAll("8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("-88.73961639404;28:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.getResults(2)
+gg.editAll("-560", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.getResults(10)
+gg.editAll("250", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("-298284466;-1.304566e23F", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("-298284466", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.getResults(99)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.setVisible(false)
+gg.getResults(1)
+gg.searchNumber("-1,883,348,481,058,764,210", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(0)
+gg.getResults(99)
+gg.editAll("-1,883,348,485,055,444,540", gg.TYPE_QWORD)
+gg.clearResults()
+gg.toast("𝙷𝙴𝙰𝙳𝙰𝙷𝙾𝚃 100% 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A6()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("10;45", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("10", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3152)
+gg.editAll("9931", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("𝚇 𝙴𝙵𝙵𝙴𝙲𝚃 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A7()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.setVisible(false)
+gg.searchNumber("-1.2382424e28;-1.4239333e28;-1.1144502e28;-1.8331474e27;-7.1608877e24::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("-1.1144502e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("7.5", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("𝚂𝙼𝙰𝙻𝙻 𝙲𝚁𝙾𝙾𝚂𝙰𝙸𝚁 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A8()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-1,883,348,481,058,764,210", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-1,883,348,481,058,764,210", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("-1,883,348,485,055,444,540", gg.TYPE_QWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("-298284466;-1.304566e23F", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-298284466", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.toast("𝙸𝙽𝚂𝚃𝙰𝙽𝚃 𝙷𝙸𝚃 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function A9()
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("0.1000000149;64.508850097", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("0.1000000149", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(50)
+gg.editAll("8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("-88.66608428955;26:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("-460", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.searchNumber("-88.73961639404;28:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("-560", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll("280", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("-29828446;-1.3046e23F", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-29828446", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("10;45", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("10", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("99", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("-1,883,348,481,058,764,210", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("-1,883,348,5,055,444,0", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("220", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.20161819458;23;25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("700", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("23;25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("23;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3)
+gg.editAll("699", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("25;23;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("180", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.1618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(15)
+gg.editAll("450", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("10;4", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("10", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("9999", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.editAll("1.10000149", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("-29828466;-1.304566e23F", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-29828466", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("16000~9999;3D;0.1;1D::40", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1600~9999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("900000", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+
+function N1()
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("220;178;15", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("220", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(440)
+gg.editAll("380", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.toast("ɪᴘᴀᴅ ᴠɪᴇᴡ 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function N2()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-369098662;0;-484380672:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("-1222129996", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.toast("ʙʟᴀᴄᴋ sᴋʏ𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function N3()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.searchNumber("-298,841,535;-409,731,072;-443,744,204;-486,469,976::13", gg.TYPE_DWORD)
+gg.refineNumber("-298,841,535", gg.TYPE_DWORD)
+gg.getResults(10)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.toast("ɴᴏ ғᴏɢ 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+function N4()
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.setVisible(false)
+gg.searchNumber("-409,731,072;-298,841,535;-409,731,072::17", gg.TYPE_DWORD)
+gg.refineNumber("-409,731,072", gg.TYPE_DWORD)
+gg.getResults(2)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Telegram : @iorihack")
+gg.toast("ʙʟᴜᴇ ғᴏɢ 𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴 ")
+end
+
+
+function STANDSCOPE()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.refineNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.refineNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1401)
+  gg.editAll("4848124999984742400", gg.TYPE_QWORD)
+  gg.clearResults()
+  gg.toast("β πππ°π½π³ ππ²πΎπΏπ΄ π°π²ππΈππ°ππ΄π³")
+end
+
+function SITSCOPE()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("18.38787841797;0.53867292404;-3.42232513428;1.77635705e-15:13", gg.TYPE_FLOAT)
+gg.refineNumber("18.38787841797", gg.TYPE_FLOAT)
+gg.getResults(181833)
+gg.editAll("130.5419921875", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+function shake()
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3042030528,
+      flags = 16,
+      value = 0
+    }
+  })
+  gg.toast("β  π°π½ππΈ-ππ·π°πΊπ΄ π°π²ππΈππ°ππ΄π³.")
+end
+
+function grass()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber(":Default__MaterialExpressionLandscapeGrassOutput", gg.TYPE_BYTE)
+  gg.getResults(500)
+  gg.editAll("0", gg.TYPE_BYTE)
+  gg.clearResults()
+  gg.toast("No Grass")
+end
+function magic()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("-2.92050004005;-86.45761108398;-88.66608428955;16;26::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-860", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("0.15122038126;90.48510742188;-88.73961639404;18;28::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("18;28::5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-960", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("9.20161819458;23;25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("200", gg.TYPE_FLOAT)
+  gg.clearResults()
+end
+function Home()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/New/main/HOME.lua')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+while true do
+  if gg.isVisible(true) then
+    PUBGMH = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if PUBGMH == 1 then
+    F2()
+  end
+end
